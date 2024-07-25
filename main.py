3fa048078e2b16cfbc19078afe0a7ea1f25037d9
Yoo Won Jeun <yoowon90@gmail.com>
2024-07-25 16:27:54 -0400

    Added time past since added to wishlist

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from website import create_app
+import datetime
 
 app = create_app()
 
+@app.template_filter('format_time')
+def format_time(timedelta: datetime.timedelta):
+    if timedelta.days > 0:
+        leftover_seconds = timedelta - datetime.timedelta(days=timedelta.days)
+        leftover_hours = leftover_seconds // 3600
+        return f"{timedelta.days} days ago"
+    
+    elif timedelta.seconds > 3600:
+            return f"{timedelta.seconds // 60} hrs ago"
+    else: 
+            return f"{timedelta.seconds} secs ago"
+            
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], port=app.config['FLASK_RUN_PORT']) 
