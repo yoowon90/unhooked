827263ad85c04e335a53005cdf28c9370691531d
Boo, Snorkel, Samspon's mom <42985078+yoowon90@users.noreply.github.com>
2025-02-08 17:41:21 -0500

    Refactored URL extraction using inspect library

--- website/url_extraction.py
+++ website/url_extraction.py
@@ -13,41 +13,59 @@ BRANDS = ['Reformation',
           'MANGO',
         ]  
 
-# TODO: Massimo Dutti, Free People, & Other Stories
-# to be added: Tiffany & Co., Vuori, ssense
-# Denied: Massimo Dutti, Ralph Lauren, Aritzia, Sezane
+# Reformation method worked with Vuori & ssense
 
-class URLInfo:
+
+# TODO: Tiffany & Co.
+
+# TODO: Denied: Free People, Massimo Dutti, Ralph Lauren, Aritzia, Sezane,  & Other Stories
+
+
+class ItemDetails:
     def __init__(self, soup):
-        self.soup = soup  # BeautifulSoup(response.text, 'html.parser')
+        # initialize default data with all Nones
         self.__default_data = {'name': None,
-                     'price': None,
-                     'description': None,
-                     'currency': None,
-                     'category': None,
-                     'brand': None}  # initialized with all Nones
+                               'price': None,
+                               'description': None,
+                               'currency': None,
+                               'category': None,
+                               'brand': None}
+        self.soup = soup  # BeautifulSoup(response.text, 'html.parser')
+        self.brands = BRANDS
+    
+    def get_extract_methods(self):
+        """Get all methods that start with 'extract_'."""
+        methods = inspect.getmembers(self, predicate=inspect.ismethod)
+        extract_methods = {name: method for name, method in methods if name.startswith('extract_')}
+        return extract_methods
 
-    def extract_item_data(self):
-        matches = {brand: dict() for brand in BRANDS}
-        for brand in BRANDS: 
-            print(f"extraction started with {brand}")
-            brand_extract_dict = self.extract_brand_from_soup(brand)
-            print(f"brand extract dict: {brand_extract_dict}")
-            name, price, description, currency, brand, category = (brand_extract_dict['name'],
+    def get_item_data(self):
+        """
+        Get item data by trying extraction methods of all brand in BRANDS.
+        Chooses the brand with the most matches of all fields, by first choosing
+        the extraction that detected a not None brand.
+        """
+        # get all extract methods
+        extract_methods = self.get_extract_methods()
+
+        # intialize dict to count # of matches for each brand
+        matches = {brand: dict() for brand in self.brands}
+
+        for brand in self.brands: 
+            print(f"Extraction started with {brand}...")
+            method_name = f'extract_{brand.lower().replace(" ", "_")}'
+            print(f"Method name for {brand} is {method_name}")
+            if method_name in extract_methods:
+                brand_extract_dict = extract_methods[method_name]()
+                print(f"brand extract dict: {brand_extract_dict}")
+                name, price, description, currency, brand, category = (brand_extract_dict['name'],
                                                          brand_extract_dict['price'],
                                                          brand_extract_dict['description'],
                                                          brand_extract_dict['currency'],
                                                          brand_extract_dict['brand'],
                                                          brand_extract_dict['category'])
-            matches[brand] = dict(name=name, price=price, description=description, currency=currency, brand=brand, category=category)
-            # if any([value is not None for value in brand_extract_dict.values()]):
-                # print("Some valid data found")
-                # return brand_extract_dict  # later comment this out
-                # break
-            # else:
-                # matches[brand] = 0
-                # print("No data found")
-        
+                matches[brand] = dict(name=name, price=price, description=description, currency=currency, brand=brand, category=category)
+
         # get the brand with the most matches. if multiple, pick one where brand_extract_dict['brand'] is equal to brand.
         # if still multiple, pick the first one
         match_counts = {brand: sum([1 for value in brand_dict.values() if value is not None]) for brand, brand_dict in matches.items()}
@@ -57,46 +75,15 @@ class URLInfo:
             return self.__default_data
         else:
             # first filter bratches that have a brand among the matches
-            brand_matches = [brand for brand, count in match_counts.items() if count == max_matches and brand is not None]
-            if len(brand_matches):
-                print(f"There has been a match and a branch match: {brand_matches[0]}")
-                return matches[brand_matches[0]]
+            brand_detections = [brand for brand, count in match_counts.items() if count == max_matches and brand is not None]
+            if len(brand_detections):
+                print(f"There has been a match and a branch detection: {brand_detections[0]}")
+                return matches[brand_detections[0]]
             else:
                 print("There has been a match, but no branch match. Using url extraction with the most matches..")
                 # get the brand with the most matches
                 best_brand = [brand for brand, count in match_counts.items() if count == max_matches][0]
                 return matches[best_brand]
-                
-
-    def extract_brand_from_soup(self, brand):
-        if brand == 'Reformation' or brand == 'American Vintage':
-            return self.extract_reformation()
-        elif brand == 'Rouje':
-            return self.extract_rouje()
-        elif brand == 'Zara':
-            return self.extract_zara()
-        elif brand == 'Massimo Dutti':
-            return self.extract_massimo_dutti()
-        elif brand == 'Aritzia':
-            return self.extract_aritzia()
-        elif brand == 'A.P.C':
-            return self.extract_apc()
-        elif brand == 'Bloomingdales':
-            return self.extract_bloomingdales()
-        elif brand == 'DÔEN':
-            return self.extract_doen()
-        elif brand == 'MANGO':
-            return self.extract_mango()
-        else:
-            return self.__default_data
-
-    def __update_data(self, new_data):
-        """ Update data with valid new data. 
-            Keep data's value as none if key was not extracted to new_data
-        """
-        for key in new_data.keys():
-            if new_data[key] is not None:
-                self.data[key] = new_data[key]
     
     def extract_reformation(self):
         """ Extracts data from Reformation website """
@@ -115,18 +102,9 @@ class URLInfo:
         finally:
             return data_copy
     
-    def extract_massimo_dutti(self):
-        # Massimo Dutti. e.g. https://www.massimodutti.com
-        data_copy = copy.deepcopy(self.__default_data)
-        try:
-            soup = self.soup
-            # use selenium
-            # https://stackoverflow.com/questions/52687372/beautifulsoup-not-returning-complete-html-of-the-page
-        
-        finally:
-            return data_copy
-    
     def extract_aritzia(self):
+        """ Extracts data from Artizia website """
+        #  TODO: Aritzia has blocked the scraping. Need to find a way to bypass it.
         data_copy = copy.deepcopy(self.__default_data)
         try:
             soup = self.soup
@@ -139,9 +117,8 @@ class URLInfo:
         finally:
             return data_copy
 
-
     def extract_rouje(self):
-        # soup.find('meta'):
+        """ Extracts data from Rouje website """
         # Rouje. e.g. https://www.rouje.com/products/daria-dress-jacquard-fleurs-rouge
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -162,6 +139,7 @@ class URLInfo:
             return data_copy
 
     def extract_zara(self):
+        """ Extracts data from Zara website """
         # zara: https://www.zara.com/us/en/ribbed-dress-p00858815.html?v1=397019608&v2=2420896
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -178,6 +156,7 @@ class URLInfo:
             return data_copy
 
     def extract_apc(self):
+        """ Extracts data from APC website """
         # apc: https://www.apc-us.com/products/blouse-julienne-vialq-f13433_aac?variant=42911449219171&currency=USD&utm_source=social&utm_medium=cpc&utm_campaign=Vervaunt_Social+%2F+CPC_APC_US_Conversion_DPA&utm_term=Remarketing_All+Remarketing+-+%28Mixed+Genders%29&utm_content=Remarketing+DPA&fbadid=120209825233840351&fbclid=PAZXh0bgNhZW0BMABhZGlkAasUnIl9ZV8BpmzeIVNQRlHAMvvI9IZnoPv4szx-vIU1wOcCbGn01e40ocq1ncWS72OwPw_aem_rQ_UuHCVYzaA5g6u_VSKTA&campaign_id=120209825200840351&ad_id=120209825233840351
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -192,6 +171,7 @@ class URLInfo:
             return data_copy
     
     def extract_bloomingdales(self):
+        """ Extracts data from Bloomingdales website """
         # bloomingdales: https://www.bloomingdales.com/shop/product/cinq-a-sept-naia-faux-shearling-jacket?ID=5274338&upc_ID=7969176&Quantity=1&seqNo=3&EXTRA_PARAMETER=BAG&pickInStore=false
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -209,6 +189,7 @@ class URLInfo:
     
     
     def extract_doen(self):
+        """ Extracts data from DÔEN website """
         # doen: https://www.bloomingdales.com/shop/product/cinq-a-sept-naia-faux-shearling-jacket?ID=5274338&upc_ID=7969176&Quantity=1&seqNo=3&EXTRA_PARAMETER=BAG&pickInStore=false
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -223,6 +204,7 @@ class URLInfo:
             return data_copy
     
     def extract_mango(self):
+        """ Extracts data from Mango website """
         # mango: https://shop.mango.com/us/en/p/women/tops/party/lurex-top-with-openwork-details_87054063?c=OR
         data_copy = copy.deepcopy(self.__default_data)
         try:
@@ -236,5 +218,4 @@ class URLInfo:
         finally:
             return data_copy
     
-     def extract_massimo_dutti(self):
-        # mango: https://shop.mango.com/us
+    
